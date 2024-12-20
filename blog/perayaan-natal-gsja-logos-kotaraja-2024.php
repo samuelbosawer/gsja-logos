730549a67b2a2b6b807ce99ea531089dc6efbdf9
Samuel Bosawer <zam.bosawer@gmail.com>
2024-12-20 20:20:08 +0900

    Update

--- blog/perayaan-natal-gsja-logos-kotaraja-2024.php
+++ blog/perayaan-natal-gsja-logos-kotaraja-2024.php
@@ -16,10 +16,10 @@
 
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-  
+
   <!-- Favicons -->
-  <link href="assets/img/favicon.png" rel="icon">
-  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+  <link href="../assets/img/favicon.png" rel="icon">
+  <link href="../assets/img/apple-touch-icon.png" rel="apple-touch-icon">
 
 
   <!-- Fonts -->
